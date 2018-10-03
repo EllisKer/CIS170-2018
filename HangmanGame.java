48f926451556cf69f0dfc73596efc7ab9cf5098f
EllisKer <43018044+EllisKer@users.noreply.github.com>
2018-10-03 19:34:34 -0400

    Add files via upload

---
+++ HangmanGame.java
@@ -0,0 +1,23 @@
+package Hangman;
+
+public class HangmanGame 
+{
+
+	//Rules that need to be followed.
+	//Game initiates.
+	//Choose word.
+	
+	public void createMask()
+	{
+		String word = "Bacon";
+		
+		for (int i = 0; i < word.length(); i++) 
+		{
+			
+			System.out.print ("*"); 
+	}
+		
+	}
+}
+
+	
